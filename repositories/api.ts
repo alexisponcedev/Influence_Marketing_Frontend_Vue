7543ad59c69ae8f3aa805a9b2dd9f5ff3a2cb944
alexisponcedev <poncealexis640@gmail.com>
2022-09-19 20:55:14 +0430

    update api.ts

--- repositories/api.ts
+++ repositories/api.ts
@@ -2290,6 +2290,10 @@ export const ConstantApiAxiosParamCreator = function (configuration?: Configurat
             const localVarHeaderParameter = {} as any;
             const localVarQueryParameter = {} as any;
 
+            // authentication bearerAuth required
+            // http bearer authentication required
+            await setBearerAuthToObject(localVarHeaderParameter, configuration)
+
 
     
             setSearchParams(localVarUrlObj, localVarQueryParameter);
