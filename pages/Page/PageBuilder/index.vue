d6a2f3a27563d5c60564bc3a000ca4c71bf37476
alexisponcedev <poncealexis640@gmail.com>
2022-09-24 14:12:51 +0330

    working on layout

---
+++ pages/Page/PageBuilder/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container fluid>
+        <v-row>
+            <v-col>
+                <v-tabs show-arrows v-model="tab" background-color="transparent">
+                    <v-tab>Products</v-tab>
+                </v-tabs>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-card>
+                    <v-card-text> Products </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import { Api } from "@/store";
+
+@Component({ layout: "page-builder" })
+export default class PageBuilder extends Vue {
+    Api = Api;
+
+    tab = "";
+}
+</script>
