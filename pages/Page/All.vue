2184d9c4318ecb908deeb8759dd9954b5b5099c0
alexisponcedev <poncealexis640@gmail.com>
2022-10-18 19:48:00 +0330

    adding product id to page all

--- pages/Page/All.vue
+++ pages/Page/All.vue
@@ -41,7 +41,8 @@ export default class AllPages extends Vue {
     headers: [
       { text: "Title", value: "title" },
       { text: "Route", value: "route" },
-      { text: "Model", value: "model_type" },
+      // { text: "Model", value: "model_type" },
+      { text: "Product", value: "model_id" },
       { text: "", value: "actions", sortable: false, width: "0" },
     ],
     actions: [
