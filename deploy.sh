3548f1ed5eead0b45f3c13af4b98d21a9c4e7a44
alexisponcedev <poncealexis640@gmail.com>
2022-10-31 17:43:30 +0330

    comment build command

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ echo "BASE_API_PATH=$BASE_API_PATH"
 sed -i "s#export const BASE_PATH = \".*\".replace#export const BASE_PATH = \"$BASE_API_PATH\".replace#" ./repositories/base.ts
 
 yarn install
-yarn build
+# yarn build
 yarn generate
