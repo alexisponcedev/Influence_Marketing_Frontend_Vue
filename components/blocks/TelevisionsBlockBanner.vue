90d1ba761ac74eb6e5ead3e37536149e98f9a2bc
alexisponcedev <poncealexis640@gmail.com>
2022-11-14 19:18:01 +0330

    save changes

---
+++ components/blocks/TelevisionsBlockBanner.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <img src="/blocks/TelevisionsBlockBanner.png" alt=""/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Prop, VModel, Watch} from "vue-property-decorator";
+import {StructureType} from "~/models/StructureType";
+
+@Component
+export default class TelevisionsBlockBanner extends Vue {
+  @Prop(Number) readonly id: number | undefined
+  @Prop({default: true}) readonly editable: Boolean | undefined
+  @VModel({type: Object}) model!: Object
+
+  reset() {
+    this.model = {
+      image : {id : 0 , type : StructureType.Image , title : "Image" , src : '' , alt : ''},
+      title : {id : 1 , type : StructureType.Text , title : 'Title' , value : ''},
+      subtitle : {id : 2 , type : StructureType.Text , title : 'Subtitle' , value : ''},
+      link : {id : 3 , type : StructureType.Url , title : 'Sign Up Now' , value : ''},
+    }
+  }
+
+  mounted() {
+    if (this.isEmpty) this.reset();
+  }
+
+  get isEmpty(): Boolean {
+    return this.model && Object.keys(this.model).length === 0;
+  }
+
+  @Watch('isEmpty')
+  onValueChanged() {
+    if (this.isEmpty) this.reset();
+  }
+}
+</script>
---
+++ components/blocks/TelevisionsBlockCards.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <img src="/blocks/TelevisionsBlockCards.png" alt=""/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Prop, VModel, Watch} from "vue-property-decorator";
+import {StructureType} from "~/models/StructureType";
+
+@Component
+export default class TelevisionsBlockCards extends Vue {
+  @Prop(Number) readonly id: number | undefined
+  @Prop({default: true}) readonly editable: Boolean | undefined
+  @VModel({type: Object}) model!: Object
+
+  reset() {
+    this.model = {
+      title: {id: 1, type: StructureType.Text, title: 'Title', value: ''},
+      subtitle: {id: 2, type: StructureType.Text, title: 'Subtitle', value: ''},
+      list: {
+        id: 3, type: StructureType.List, title: "List",
+        newItem: {
+          title: {id: 1, type: StructureType.Text, title: 'Title', value: ''},
+          subtitle: {id: 2, type: StructureType.Text, title: 'Subtitle', value: ''},
+          image: {id: 2, type: StructureType.Image, title: 'Image', src: '', alt: ''},
+          link: {id: 2, type: StructureType.Url, title: 'Link', value: ''},
+        },
+        value: [
+          {
+            title: {id: 1, type: StructureType.Text, title: 'Title', value: ''},
+            subtitle: {id: 2, type: StructureType.Text, title: 'Subtitle', value: ''},
+            image: {id: 2, type: StructureType.Image, title: 'Image', src: '', alt: ''},
+            link: {id: 2, type: StructureType.Url, title: 'Link', value: ''},
+          }
+        ]
+      }
+    }
+  }
+
+  mounted() {
+    if (this.isEmpty) this.reset();
+  }
+
+  get isEmpty(): Boolean {
+    return this.model && Object.keys(this.model).length === 0;
+  }
+
+  @Watch('isEmpty')
+  onValueChanged() {
+    if (this.isEmpty) this.reset();
+  }
+}
+</script>
