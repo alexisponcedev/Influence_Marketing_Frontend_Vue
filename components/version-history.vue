24d0445d7a45fd18b98a8ad239bad9be42c9c048
alexisponcedev <poncealexis640@gmail.com>
2022-11-19 16:05:35 +0330

    save changes befor add login protal

--- components/version-history.vue
+++ components/version-history.vue
@@ -11,13 +11,13 @@
 
 
             <div v-if="Api.History.loading" class="tw-grid tw-grid-cols-1 tw-gap-3">
-                <div v-for="i in 5" :key="i" class="tw-text-sm tw-transition tw-duration-300 tw-border tw-border-solid tw-border-white tw-bg-gray-50 tw-p-3 tw-rounded-lg tw-animate-pulse ">
-                    <div class="tw-w-40 tw-h-4 tw-bg-gray-100 tw-rounded-lg" />
-                    <div class="tw-w-64 tw-h-4 tw-bg-gray-100 tw-rounded-lg tw-mt-1" />
+                <div v-for="i in 5" :key="i"
+                     class="tw-text-sm tw-transition tw-duration-300 tw-border tw-border-solid tw-border-white tw-bg-gray-50 tw-p-3 tw-rounded-lg tw-animate-pulse ">
+                    <div class="tw-w-40 tw-h-4 tw-bg-gray-100 tw-rounded-lg"/>
+                    <div class="tw-w-64 tw-h-4 tw-bg-gray-100 tw-rounded-lg tw-mt-1"/>
                 </div>
 
             </div>
-
 
 
             <ul v-else class=" tw-list-none tw-pl-0 tw-space-y-2" style="padding-left : 0;">
@@ -25,7 +25,6 @@
                 <li v-for="history in Api.History.all" :key="history.id"
                     :class="{'tw-bg-white tw-border-gray-600' : selected.id === history.id}"
                     class="tw-text-sm tw-transition tw-duration-300 tw-border tw-border-solid tw-border-white tw-bg-gray-50 tw-p-3 tw-rounded-lg tw-cursor-pointer">
-
                     <div class="tw-flex tw-items-center tw-space-x-2">
 
                         <div class="tw-flex-1" @click="select(history)">
@@ -36,16 +35,35 @@
                             <datetime class="tw-mt-1 tw-text-gray-500" :value="history.created_at"/>
                         </div>
 
-                        <div v-if="history.id === selected.id">
-                            <button @click.self="preview(history)" class="">preview</button>
+                        <div v-if="history.id === selected.id"
+                             class="tw-flex tw-flex-col tw-justify-center tw-items-center tw-space-y-0.5">
+                            <button @click.self="preview(history)" class="hover:tw-text-blue-500">preview</button>
+                            <button @click.self="saving = true" class="hover:tw-text-blue-500">save</button>
                         </div>
                     </div>
-
                 </li>
 
             </ul>
         </div>
 
+
+        <v-dialog v-model="saving">
+            <v-card>
+                <v-card-title>
+                    History Manager Manager
+                </v-card-title>
+
+                <v-card-text>
+                    <form-field-text :field="titleField" v-model="selected.name"/>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn :disabled="saving" @click="save" text color="green">Save</v-btn>
+                    <v-btn :disabled="saving" @click="cancel" text color="red"> Cancel</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
 
     </v-navigation-drawer>
 </template>
@@ -67,7 +85,16 @@ export default class VersionHistory extends Vue {
 
     selected: any = {};
 
+    saving: boolean = false;
+
     loading: boolean = false;
+
+    titleField = {
+        label: 'History Name',
+        placeholder: 'Enter history name',
+        rules: [],
+        colAttrs: {cols: 12}
+    }
 
     open() {
         this.loadHistory();
@@ -83,7 +110,7 @@ export default class VersionHistory extends Vue {
     }
 
     preview(history: any) {
-        this.$emit('input' , history.widgets);
+        this.$emit('input', history.widgets);
         this.close();
     }
 
@@ -91,5 +118,25 @@ export default class VersionHistory extends Vue {
         await Api.History.getHistoryByModelNameModelId({model_id: +this.model.id!, model_name: this.type})
     }
 
+    save(){
+
+    }
+
+    cancel(){
+        this.saving = false;
+    }
+
 }
 </script>
+
+<style scoped>
+.x-input {
+    height: 39px;
+    width: 100%;
+    text-align: center;
+}
+
+.x-btn {
+    @apply tw-w-full tw-py-1 tw-px-2 tw-bg-gray-50 tw-rounded-lg
+}
+</style>
--- pages/page/edit/_id/page-builder.vue
+++ pages/page/edit/_id/page-builder.vue
@@ -1,89 +1,91 @@
 <template>
-  <v-container fluid style="background-color: #F2F3F8;height: 100vh">
+    <v-container fluid style="background-color: #F2F3F8;height: 100vh">
 
-    <v-card color="#FCFCFC" elevation="0" class="mb-4 px-7 page-builder-header">
-      <v-row align="center">
+        <v-card color="#FCFCFC" elevation="0" class="mb-4 px-7 page-builder-header">
+            <v-row align="center">
 
-        <v-col cols="12" md="5">
-          <div class="tw-flex tw-space-x-2 tw-items-center">
-            <button @click="discard">
-              <v-icon color="black" large>mdi-close</v-icon>
-            </button>
+                <v-col cols="12" md="5">
+                    <div class="tw-flex tw-space-x-2 tw-items-center">
+                        <button @click="discard">
+                            <v-icon color="black" large>mdi-close</v-icon>
+                        </button>
 
-            <div>
-              <h1 class="text-h6 font-weight-bold mb-1">Page builder</h1>
-              <span class="text-subtitle-2 grey--text text--darken-2">{{ Page.title }}</span>
-            </div>
+                        <div>
+                            <h1 class="text-h6 font-weight-bold mb-1">Page builder</h1>
+                            <span class="text-subtitle-2 grey--text text--darken-2">{{ Page.title }}</span>
+                        </div>
 
-          </div>
+                    </div>
 
-        </v-col>
-        <v-col cols="12" md="7" class="text-right">
+                </v-col>
+                <v-col cols="12" md="7" class="text-right">
 
-          <v-btn @click="openHistory" elevation="0" outlined color="grey darken-4" class="control-btns">
-            History
-          </v-btn>
+                    <v-btn @click="openHistory" elevation="0" outlined color="grey darken-4" class="control-btns">
+                        <v-icon>mdi-history</v-icon>
+                        History
+                    </v-btn>
 
-          <v-btn @click="gotoLiveWebsite" elevation="0" outlined color="grey darken-4" class="control-btns">
-            <v-icon>mdi-play-circle</v-icon>
-            Preview
-          </v-btn>
-
-
-          <v-btn v-if="shouldDeploy" @click="saveAndDeploy" elevation="0" color="grey darken-4 white--text"
-                 class="control-btns">
-            Save and Deploy
-          </v-btn>
-
-          <v-btn v-else @click="savePage" elevation="0" color="grey darken-4 white--text" class="control-btns">
-            Save Page
-          </v-btn>
+                    <v-btn @click="gotoLiveWebsite" elevation="0" outlined color="grey darken-4" class="control-btns">
+                        <v-icon>mdi-play-circle</v-icon>
+                        Preview
+                    </v-btn>
 
 
-          <v-menu bottom offset-x="-10" offset-y="12">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn icon elevation="0" v-on="on" v-bind="attrs">
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </template>
+                    <v-btn v-if="shouldDeploy" @click="saveAndDeploy" elevation="0" color="grey darken-4 white--text"
+                           class="control-btns">
+                        Save and Deploy
+                    </v-btn>
 
-            <v-list>
-              <v-list-item>
-                <v-list-item-title>
-                  <button class="tw-block tw-p-1.5" @click="saveTemplate">Save as template</button>
-                </v-list-item-title>
-              </v-list-item>
-
-              <v-list-item>
-                <v-list-item-title>
-                  <button class="tw-block tw-p-1.5" @click="saveDraft">Save Draft</button>
-                </v-list-item-title>
-              </v-list-item>
-
-              <v-divider></v-divider>
-
-              <v-list-item>
-                <v-list-item-title>
-                  <button class="tw-block tw-px-2 tw-py-1.5" @click="discard">Discard</button>
-                </v-list-item-title>
-              </v-list-item>
+                    <v-btn v-else @click="savePage" elevation="0" color="grey darken-4 white--text"
+                           class="control-btns">
+                        Save Page
+                    </v-btn>
 
 
-            </v-list>
-          </v-menu>
+                    <v-menu bottom offset-x="-10" offset-y="12">
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn icon elevation="0" v-on="on" v-bind="attrs">
+                                <v-icon>mdi-dots-vertical</v-icon>
+                            </v-btn>
+                        </template>
 
-        </v-col>
-      </v-row>
-    </v-card>
+                        <v-list>
+                            <v-list-item>
+                                <v-list-item-title>
+                                    <button class="tw-block tw-p-1.5" @click="saveTemplate">Save as template</button>
+                                </v-list-item-title>
+                            </v-list-item>
 
-    <page-builder v-model="blocksList" @needDeploy="needDeploy"/>
+                            <v-list-item>
+                                <v-list-item-title>
+                                    <button class="tw-block tw-p-1.5" @click="saveDraft">Save Draft</button>
+                                </v-list-item-title>
+                            </v-list-item>
 
-    <template-selector ref="templateManager"/>
+                            <v-divider></v-divider>
 
-    <version-history ref="history" type="page"  :value="Page" @input="blocks => blocksList = blocks"/>
+                            <v-list-item>
+                                <v-list-item-title>
+                                    <button class="tw-block tw-px-2 tw-py-1.5" @click="discard">Discard</button>
+                                </v-list-item-title>
+                            </v-list-item>
 
-    <loading-overlay :show="Api.Page.loading"/>
-  </v-container>
+
+                        </v-list>
+                    </v-menu>
+
+                </v-col>
+            </v-row>
+        </v-card>
+
+        <page-builder v-model="blocksList" @needDeploy="needDeploy"/>
+
+        <template-selector ref="templateManager"/>
+
+        <version-history ref="history" type="page" :value="Page" @input="blocks => blocksList = blocks"/>
+
+        <loading-overlay :show="Api.Page.loading"/>
+    </v-container>
 </template>
 
 <script lang="ts">
@@ -93,77 +95,78 @@ import {Page, Widgets} from "~/repositories";
 import {BlockInterface} from "~/interfaces/BlockInterface";
 import {SettingEnum} from "~/interfaces/SettingEnum";
 import VersionHistory from "~/components/version-history.vue";
+
 @Component({
-  components: {VersionHistory}
+    components: {VersionHistory}
 })
 export default class PageBuilderSection extends Vue {
-  Api = Api;
+    Api = Api;
 
-  tab = "";
+    tab = "";
 
-  blocksList: BlockInterface[] = [];
+    blocksList: BlockInterface[] = [];
 
-  Page: Page = {};
+    Page: Page = {};
 
-  shouldDeploy: Boolean = false;
+    shouldDeploy: Boolean = false;
 
-  livePreviewUrl = '';
+    livePreviewUrl = '';
 
-  // drawer: boolean = true;
+    // drawer: boolean = true;
 
-  async mounted() {
-    this.Page = (await Api.Page.get(+this.$route.params.id)) as Page;
-    if (this.Page.draft && this.Page.draft.length > 0)
-      this.blocksList = this.Page.draft as Array<BlockInterface>;
-    else
-      this.blocksList = (this.Page.widgets ? this.Page.widgets : []) as Array<BlockInterface>;
+    async mounted() {
+        this.Page = (await Api.Page.get(+this.$route.params.id)) as Page;
+        if (this.Page.draft && this.Page.draft.length > 0)
+            this.blocksList = this.Page.draft as Array<BlockInterface>;
+        else
+            this.blocksList = (this.Page.widgets ? this.Page.widgets : []) as Array<BlockInterface>;
 
-    Api.Setting.getValue(SettingEnum.livePreview).then(value => {
-      this.livePreviewUrl = value ? value : '';
-    })
-  }
+        Api.Setting.getValue(SettingEnum.livePreview).then(value => {
+            this.livePreviewUrl = value ? value : '';
+        })
+    }
 
-  discard() {
-    this.$router.push('/page/edit/' + this.Page.id);
-  }
+    discard() {
+        this.$router.push('/page/edit/' + this.Page.id);
+    }
 
 
-  async savePage() {
-    let widgets: Widgets = {page_id: +this.$route.params.id, widgets: this.blocksList}
-    await Api.Page.savePageWidgets(widgets);
-  }
+    async savePage() {
+        let widgets: Widgets = {page_id: +this.$route.params.id, widgets: this.blocksList}
+        await Api.Page.savePageWidgets(widgets);
+    }
 
-  async saveAndDeploy() {
-    this.savePage().then(Api.Page.doDeploy).then(() => {
-      this.shouldDeploy = false;
-    })
-  }
+    async saveAndDeploy() {
+        this.savePage().then(Api.Page.doDeploy).then(() => {
+            this.shouldDeploy = false;
+        })
+    }
 
-  get liveWebsite() {
-    return this.livePreviewUrl + this.Page.route
-  }
+    get liveWebsite() {
+        return this.livePreviewUrl + this.Page.route
+    }
 
-  gotoLiveWebsite() {
-    window.open(this.liveWebsite, '_blank');
-  }
+    gotoLiveWebsite() {
+        window.open(this.liveWebsite, '_blank');
+    }
 
-  saveTemplate() {
-    (this.$refs.templateManager as any).open(true, this.blocksList);
-  }
+    saveTemplate() {
+        (this.$refs.templateManager as any).open(true, this.blocksList);
+    }
 
-  async saveDraft() {
-    await Api.Page.saveDraft({page_id: +this.$route.params.id, page_draft: this.blocksList})
-  }
+    async saveDraft() {
+        await Api.Page.saveDraft({page_id: +this.$route.params.id, page_draft: this.blocksList})
+    }
 
-  needDeploy() {
-    console.log('needDeploy fired');
-    this.shouldDeploy = true
-  }
+    needDeploy() {
+        console.log('needDeploy fired');
+        this.shouldDeploy = true
+    }
 
-  openHistory() {
-    // load history data first
-    (this.$refs.history as any).open();
-    // this.drawer = true;
-  }
+    openHistory() {
+        // load history data first
+        (this.$refs.history as any).open();
+        // this.drawer = true;
+    }
 }
 </script>
