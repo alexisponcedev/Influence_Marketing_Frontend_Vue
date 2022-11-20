94b959f5754ecca8b13ec40382603fb56642b604
alexisponcedev <poncealexis640@gmail.com>
2022-11-20 14:46:34 +0330

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-BASE_API_PATH="https://imcxm.exodusapi.influencedev.com/api"
+BASE_API_PATH="https://imcxm.stage-api.hisenseportal.com/api"
 export $(grep -v '^#' .env | xargs -d '\r\n')
 echo "BASE_API_PATH=$BASE_API_PATH"
 
 sed -i "s#export const BASE_PATH = \".*\".replace#export const BASE_PATH = \"$BASE_API_PATH\".replace#" ./repositories/base.ts
 
 yarn install
-yarn build
 yarn generate
