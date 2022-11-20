593df13671210257dd0a85d9d7bd4ee770054d87
alexisponcedev <poncealexis640@gmail.com>
2022-11-20 10:05:21 +0330

    update api.ts

--- repositories/api.ts
+++ repositories/api.ts
@@ -602,6 +602,25 @@ export interface History {
     'widgets'?: Array<object>;
 }
 /**
+ * HistoryRequest body data
+ * @export
+ * @interface HistoryRequest
+ */
+export interface HistoryRequest {
+    /**
+     * 
+     * @type {number}
+     * @memberof HistoryRequest
+     */
+    'id'?: number;
+    /**
+     * 
+     * @type {string}
+     * @memberof HistoryRequest
+     */
+    'title'?: string;
+}
+/**
  * History Resource model
  * @export
  * @interface HistoryResource
@@ -631,6 +650,18 @@ export interface HistoryResource {
      * @memberof HistoryResource
      */
     'widgets'?: Array<object>;
+    /**
+     * 
+     * @type {string}
+     * @memberof HistoryResource
+     */
+    'created_at'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof HistoryResource
+     */
+    'updated_at'?: string;
 }
 /**
  * 
@@ -1455,10 +1486,13 @@ export interface Page {
      * @type {Array<object>}
      * @memberof Page
      */
-    'widgets'?: Array<object>;
-
     'draft'?: Array<object>;
-
+    /**
+     * 
+     * @type {Array<object>}
+     * @memberof Page
+     */
+    'widgets'?: Array<object>;
     /**
      * 
      * @type {number}
@@ -3451,6 +3485,10 @@ export const AuthApiAxiosParamCreator = function (configuration?: Configuration)
             const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
             const localVarHeaderParameter = {} as any;
             const localVarQueryParameter = {} as any;
+
+            // authentication bearerAuth required
+            // http bearer authentication required
+            await setBearerAuthToObject(localVarHeaderParameter, configuration)
 
 
     
@@ -6768,6 +6806,45 @@ export const HistoryApiAxiosParamCreator = function (configuration?: Configurati
         },
         /**
          * 
+         * @param {HistoryRequest} historyRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        historyTitleUpdate: async (historyRequest: HistoryRequest, options: AxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'historyRequest' is not null or undefined
+            assertParamExists('historyTitleUpdate', 'historyRequest', historyRequest)
+            const localVarPath = `/history/titleUpdate`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'PUT', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication bearerAuth required
+            // http bearer authentication required
+            await setBearerAuthToObject(localVarHeaderParameter, configuration)
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            localVarRequestOptions.data = serializeDataIfNeeded(historyRequest, localVarRequestOptions, configuration)
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
+         * 
          * @param {number} id History ID
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
@@ -6845,6 +6922,16 @@ export const HistoryApiFp = function(configuration?: Configuration) {
         },
         /**
          * 
+         * @param {HistoryRequest} historyRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async historyTitleUpdate(historyRequest: HistoryRequest, options?: AxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<any>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.historyTitleUpdate(historyRequest, options);
+            return createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration);
+        },
+        /**
+         * 
          * @param {number} id History ID
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
@@ -6889,6 +6976,15 @@ export const HistoryApiFactory = function (configuration?: Configuration, basePa
          */
         getHistoryByModelNameModelId(modelName: string, modelId: number, options?: any): AxiosPromise<InlineResponse20025> {
             return localVarFp.getHistoryByModelNameModelId(modelName, modelId, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * 
+         * @param {HistoryRequest} historyRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        historyTitleUpdate(historyRequest: HistoryRequest, options?: any): AxiosPromise<any> {
+            return localVarFp.historyTitleUpdate(historyRequest, options).then((request) => request(axios, basePath));
         },
         /**
          * 
@@ -6940,6 +7036,17 @@ export class HistoryApi extends BaseAPI {
      */
     public getHistoryByModelNameModelId(modelName: string, modelId: number, options?: AxiosRequestConfig) {
         return HistoryApiFp(this.configuration).getHistoryByModelNameModelId(modelName, modelId, options).then((request) => request(this.axios, this.basePath));
+    }
+
+    /**
+     * 
+     * @param {HistoryRequest} historyRequest 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof HistoryApi
+     */
+    public historyTitleUpdate(historyRequest: HistoryRequest, options?: AxiosRequestConfig) {
+        return HistoryApiFp(this.configuration).historyTitleUpdate(historyRequest, options).then((request) => request(this.axios, this.basePath));
     }
 
     /**
