c1fa03231bd277c38e01a64691c5421ae3e7fcd6
alexisponcedev <poncealexis640@gmail.com>
2022-11-30 15:37:33 +0330

    fix bugs - redirection set page_id by default from redirection page

--- components/structure/editor/url.vue
+++ components/structure/editor/url.vue
@@ -1,26 +1,27 @@
 <template>
-    <div class="tw-w-full" :class="{'tw-px-2' : hasBackground}">
+    <div class="tw-w-full" :class="{ 'tw-px-2': hasBackground }">
         <div class="tw-mb-1" v-if="showTitle"> {{ model.title }}</div>
 
-        <div class=""
-             :class="{ 'tw-flex tw-space-x-2' : inline ,
-         'tw-border tw-border-solid tw-border-gray-200 tw-rounded-lg tw-pt-1' : hasBackground}"
-             :style="{'backgroundColor': hasBackground ?  '#fdfdfd' : 'transparent'}">
+        <div class="" :class="{
+            'tw-flex tw-space-x-2': inline,
+            'tw-border tw-border-solid tw-border-gray-200 tw-rounded-lg tw-pt-1': hasBackground
+        }"
+            :style="{ 'backgroundColor': hasBackground ? '#fdfdfd' : 'transparent' }">
 
-            <form-field-text v-if="showTitle" :field="titleField" v-model="model.title"/>
+            <form-field-text v-if="showTitle" :field="titleField" v-model="model.title" />
 
-            <form-field-select :field="typeField" v-model="type" @change="prepare"/>
+            <form-field-select :field="typeField" v-model="type" @change="prepare" />
 
-            <form-field-select-page-name v-if="type === UrlTypeEnum.Internal" :field="selectField" v-model="route"/>
+            <form-field-select-page-name v-if="type === UrlTypeEnum.Internal" :field="selectField" v-model="route" @update:selected-page="$emit('page-selected' , $event)" />
 
-            <form-field-text v-if="type === UrlTypeEnum.Internal" :field="queryField" v-model="query"/>
+            <form-field-text v-if="type === UrlTypeEnum.Internal" :field="queryField" v-model="query" />
 
-            <form-field-text v-if="type === UrlTypeEnum.Custom" :field="urlFiled" v-model="model.value"/>
+            <form-field-text v-if="type === UrlTypeEnum.Custom" :field="urlFiled" v-model="model.value" />
 
             <form-field-text v-if="type === UrlTypeEnum.openChannelAdvisor" :field="productField"
-                             v-model="productModel"/>
+                v-model="productModel" />
 
-            <structure-editor-id-selector v-if="type === UrlTypeEnum.anchor" :field="anchorField" v-model="model"/>
+            <structure-editor-id-selector v-if="type === UrlTypeEnum.anchor" :field="anchorField" v-model="model" />
         </div>
         <p class="tw-text-blue-500 tw-my-1 tw-pl-1">{{ model.value }}</p>
 
@@ -29,10 +30,10 @@
 </template>
 
 <script lang="ts">
-import {Component, Prop, VModel, Vue, Watch} from "vue-property-decorator";
-import {StructureField} from "~/interfaces/StructureField";
-import {UrlTypeEnum} from "~/interfaces/UrlTypeEnum";
-import {Api} from "~/utils/store-accessor";
+import { Component, Prop, VModel, Vue, Watch } from "vue-property-decorator";
+import { StructureField } from "~/interfaces/StructureField";
+import { UrlTypeEnum } from "~/interfaces/UrlTypeEnum";
+import { Api } from "~/utils/store-accessor";
 
 
 @Component
@@ -40,21 +41,21 @@ export default class StructureUrlEditor extends Vue {
 
     @Prop({
         type: Array, default: () => [
-            {title: 'Page URLs', value: UrlTypeEnum.Internal},
-            {title: 'Custom URL', value: UrlTypeEnum.Custom},
-            {title: 'Open Channel Advisor', value: UrlTypeEnum.openChannelAdvisor},
-            {title: 'Section or Anchor', value: UrlTypeEnum.anchor}
+            { title: 'Page URLs', value: UrlTypeEnum.Internal },
+            { title: 'Custom URL', value: UrlTypeEnum.Custom },
+            { title: 'Open Channel Advisor', value: UrlTypeEnum.openChannelAdvisor },
+            { title: 'Section or Anchor', value: UrlTypeEnum.anchor }
         ]
     }) options!: any
     @Prop({
         type: Object, default: () => {
         }
     }) rebuild!: any
-    @Prop({type: Boolean, default: true}) hasBackground!: Boolean
-    @Prop({type: Boolean, default: false}) disableTitle!: Boolean
-    @Prop({type: Boolean, default: true}) showTitle!: Boolean
-    @Prop({type: Boolean, default: false}) inline!: Boolean
-    @VModel({type: StructureField}) model!: StructureField
+    @Prop({ type: Boolean, default: true }) hasBackground!: Boolean
+    @Prop({ type: Boolean, default: false }) disableTitle!: Boolean
+    @Prop({ type: Boolean, default: true }) showTitle!: Boolean
+    @Prop({ type: Boolean, default: false }) inline!: Boolean
+    @VModel({ type: StructureField }) model!: StructureField
 
     UrlTypeEnum = UrlTypeEnum;
 
@@ -72,7 +73,7 @@ export default class StructureUrlEditor extends Vue {
         rules: [],
         'item-text': 'title',
         'item-value': 'value',
-        colAttrs: {cols: this.inline ? 2 : 12},
+        colAttrs: { cols: this.inline ? 2 : 12 },
         items: this.options
     }
 
@@ -81,21 +82,21 @@ export default class StructureUrlEditor extends Vue {
         placeholder: 'Enter Title',
         rules: [],
         disabled: this.disableTitle,
-        colAttrs: {cols: this.inline ? 3 : 12},
+        colAttrs: { cols: this.inline ? 3 : 12 },
     }
 
     anchorField = {
         label: "Select Section",
         placeholder: 'Select a Section',
         rules: [],
-        colAttrs: {cols: this.inline ? 6 : 12},
+        colAttrs: { cols: this.inline ? 6 : 12 },
     }
 
     productField = {
         label: "Product Model",
         placeholder: 'Enter Product Model',
         rules: [],
-        colAttrs: {cols: this.inline ? 6 : 12},
+        colAttrs: { cols: this.inline ? 6 : 12 },
     }
 
     selectField = {
@@ -104,21 +105,21 @@ export default class StructureUrlEditor extends Vue {
         'item-text': 'title',
         'item-value': 'route',
         rules: [],
-        colAttrs: {cols: this.inline ? 3 + (this.showTitle ? 0 : 3) : 12},
+        colAttrs: { cols: this.inline ? 3 + (this.showTitle ? 0 : 3) : 12 },
     }
 
     urlFiled = {
         label: "Custom URL",
         placeholder: 'Enter a valid url',
         rules: [],
-        colAttrs: {cols: this.inline ? 6 : 12},
+        colAttrs: { cols: this.inline ? 6 : 12 },
     }
 
     queryField = {
         label: "Query params (optional)",
         placeholder: 'filter=55&category_id=3...',
         rules: [],
-        colAttrs: {cols: this.inline ? 3 : 12},
+        colAttrs: { cols: this.inline ? 3 : 12 },
     }
 
     async prepare() {
@@ -171,19 +172,19 @@ export default class StructureUrlEditor extends Vue {
         this.model.value = 'openChannelAdvisor:' + this.productModel.toUpperCase()
     }
 
-    @Watch('value', {deep: true, immediate: true})
+    @Watch('value', { deep: true, immediate: true })
     onValueChanged(value: any, oldValue: any) {
         this.model = value;
         this.prepare()
         // .then(this.updateType);
     }
 
-    @Watch('model', {immediate: true, deep: true})
+    @Watch('model', { immediate: true, deep: true })
     onModelUpdated() {
         this.$emit('update:url', this.model);
     }
 
-    @Watch('rebuild', {immediate: true, deep: true})
+    @Watch('rebuild', { immediate: true, deep: true })
     onRebuild() {
         console.log('on rebuild  fired:')
         // this.updateType()
