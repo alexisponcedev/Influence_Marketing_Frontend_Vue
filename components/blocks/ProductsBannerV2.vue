de63239734137f4351e821632528c48f7b9da2a8
alexisponcedev <poncealexis640@gmail.com>
2022-12-20 11:15:26 +0330

    update productbannerv2 structur using safe way to update

--- components/blocks/ProductsBannerV2.vue
+++ components/blocks/ProductsBannerV2.vue
@@ -5,67 +5,49 @@
 </template>
 
 <script lang="ts">
-import {Vue, Component, Prop, VModel, Watch} from "vue-property-decorator";
-import {StructureType} from "~/models/StructureType";
-import {Theme} from "~/interfaces/ThemeEnum";
+import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
+import { StructureType } from "~/models/StructureType";
+import { Theme } from "~/interfaces/ThemeEnum";
+import blockAddItem from "~/utils/blockAddItem";
 
 
 @Component
 export default class ProductsBannerV2 extends Vue {
     @Prop(Number) readonly id: number | undefined
     @Prop(Number) readonly product_id!: number
-    @VModel({type: Object}) model!: any
+    @VModel({ type: Object }) model!: any
 
     Theme = Theme;
 
     product: Object = {};
     loadingProduct: boolean = true;
 
-    reset(oldValue: any = {}) {
-
-        if (oldValue && Object.keys(oldValue).length > 0) {
-            this.model = {
-                ...oldValue, ...{
-                    backgroundColor: {id: 7, type: StructureType.Color, title: 'Background color', value: '#fff'}
-                }
-            }
-        } else
-            this.model = {
-                theme: {
-                    id: 0,
-                    type: StructureType.Select,
-                    title: 'Theme',
-                    value: Theme.dark,
-                    items: [
-                        {title: 'Light', value: this.Theme.light},
-                        {title: 'Dark', value: this.Theme.dark},
-                    ]
-                },
-                image: {
-                    id: 1,
-                    type: StructureType.Image,
-                    title: 'Image',
-                    src: 'https://assets.hisense-usa.com/assets/ContentBuilderImages/39f6c6b03f/content_dp-beautiful-screen-min-clikhq.png',
-                    alt: 'Some note about this image',
-                },
-                title: {id: 2, type: StructureType.Text, title: 'Title', value: 'Televisions'}
-            }
-    }
-
     mounted() {
-        if (this.isEmpty) this.reset();
+        blockAddItem(this.model, 'theme', {
+            id: 0,
+            type: StructureType.Select,
+            title: 'Theme',
+            value: Theme.dark,
+            items: [
+                { title: 'Light', value: this.Theme.light },
+                { title: 'Dark', value: this.Theme.dark },
+            ]
+        })
+        blockAddItem(this.model, 'image', {
+            id: 1,
+            type: StructureType.Image,
+            title: 'Image',
+            src: 'https://assets.hisense-usa.com/assets/ContentBuilderImages/39f6c6b03f/content_dp-beautiful-screen-min-clikhq.png',
+            alt: 'Some note about this image',
+        })
+        blockAddItem(this.model, 'title', { id: 2, type: StructureType.Text, title: 'Title', value: 'Televisions' })
 
+        this.model = { ... this.model }
     }
 
 
     get isEmpty(): Boolean {
         return this.model && Object.keys(this.model).length === 0;
-    }
-
-    @Watch('isEmpty')
-    onValueChanged() {
-
-        if (this.isEmpty) this.reset();
     }
 }
 </script>
