f136726b114738d0a47bf8b754b572e1baa9654d
alexisponcedev <poncealexis640@gmail.com>
2022-12-21 14:48:44 +0330

    add ProductfeatureWithImage Component

--- components/blocks/ProductFeatureWithImage.vue
+++ components/blocks/ProductFeatureWithImage.vue
@@ -28,6 +28,7 @@ import {Vue, Component, Prop, VModel, Watch} from "vue-property-decorator";
 import {StructureType} from "~/models/StructureType";
 import {Theme} from "~/interfaces/ThemeEnum";
 import {Direction} from "~/interfaces/DirectionEnum";
+import blockAddItem from "~/utils/blockAddItem";
 
 @Component
 export default class BlockProductFeatureWithImage extends Vue {
@@ -107,7 +108,60 @@ export default class BlockProductFeatureWithImage extends Vue {
     }
 
     mounted() {
-        if (this.isEmpty) this.reset();
+
+        blockAddItem(this.model , 'theme' , {
+                    id: 0,
+                    type: StructureType.Select,
+                    title: "Theme",
+                    value: Theme.dark,
+                    items: [
+                        {title: "Light", value: this.Theme.light},
+                        {title: "Dark", value: this.Theme.dark},
+                    ],
+                });
+        blockAddItem(this.model , 'direction' , {
+                    id: 1,
+                    type: StructureType.Select,
+                    title: "Direction",
+                    value: Direction.ltr,
+                    items: [
+                        {title: "Left To Right", value: this.Direction.ltr},
+                        {title: "Right To Left", value: this.Direction.rtl},
+                    ],
+                });
+        blockAddItem(this.model , 'littleTitle' , {
+                    id: 2,
+                    type: StructureType.String,
+                    title: 'Little Title',
+                    value: "4K ULED ™"
+                });
+        blockAddItem(this.model , 'title' , {
+                    id: 3,
+                    type: StructureType.String,
+                    title: 'Title',
+                    value: "Sharper, smoother pictures"
+                });
+        blockAddItem(this.model , 'description' , {
+                    id: 4,
+                    type: StructureType.String,
+                    title: 'Description',
+                    value: "4K greatness, but better. The U8G has our exclusive ULED technologies. They boost color, contrast, brightness, motion… we could go on. It’s the TV your old TV wants to be."
+                });
+                blockAddItem(this.model , 'image' , {
+                    id: 5,
+                    type: StructureType.Image,
+                    title: 'Image',
+                    src: 'https://assets.hisense-usa.com/assets/ContentBuilderImages/26632e121d/pdp-3_6-u9-50-50-feat-1__ScaleMaxWidthWzMwNDhd.jpg-t34p4p.jpg',
+                    alt: 'featured image'
+                });
+                blockAddItem(this.model , 'link' , {
+                    id: 6,
+                    type: StructureType.Url,
+                    title: 'Shop TV',
+                    value: "/"
+                });
+                blockAddItem(this.model , 'backgroundColor' , {id: 7, type: StructureType.Color, title: 'Background color', value: '#fff'});
+                blockAddItem(this.model , 'textColor' , {id: 7, type: StructureType.Color, title: 'Text color', value: '#fff'});
     }
 
     get isEmpty(): Boolean {
