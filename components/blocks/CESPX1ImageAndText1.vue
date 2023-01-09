27461386cbcc6b39ccbdff2787d729be713013d1
alexisponcedev <poncealexis640@gmail.com>
2023-01-09 15:59:57 +0330

    .

---
+++ components/blocks/CESPX1ImageAndText1.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <img src="/blocks/CESPX1ImageAndText1.png" alt=""/>
+    </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Prop, VModel, Watch} from "vue-property-decorator";
+import {StructureType} from "~/models/StructureType";
+
+@Component
+export default class CesPX1ImageAndText1 extends Vue {
+    @Prop(Number) readonly id: number | undefined
+    @Prop({default: true}) readonly editable: Boolean | undefined
+    @VModel({type: Object}) model!: any
+
+    reset(oldValue: any = {}) {
+
+        if (oldValue && Object.keys(oldValue).length > 0) {
+            this.model = {
+                ...oldValue, ...{
+                    backgroundColor: {id: 7, type: StructureType.Color, title: 'Background color', value: '#fff'}
+                }
+            }
+        } else
+            this.model = {
+                text: {
+                    id: 0,
+                    type: StructureType.Text,
+                    title: 'Text',
+                    value: ''
+                },
+                slider: {
+                    id: 1, type: StructureType.List, title: 'Slider Images',
+                    newItem: {
+                        image: {id: 0, type: StructureType.Image, title: 'Image', src: '', alt: 'Image Alt'},
+                    },
+                    value: [
+                        {
+                            image: {id: 0, type: StructureType.Image, title: 'Image', src: '', alt: 'Image Alt'},
+                        }
+                    ]
+                }
+
+            }
+    }
+
+    mounted() {
+        if (this.isEmpty) this.reset();
+    }
+
+    get isEmpty(): Boolean {
+        return this.model && Object.keys(this.model).length === 0;
+    }
+
+    addItem(name: string, item: any) {
+        if (!this.model.hasOwnProperty(name)) this.model[name] = item;
+        this.model[name].id = item.id;
+
+        if (this.model[name].type !== item.type) this.model[name].type = item.type;
+        if (item.type === StructureType.Image) {
+            this.model[name].src = '';
+            this.model[name].alt = 'Image Alt';
+        }
+        if (item.type === StructureType.List) {
+            this.model[name].newItem = item.newItem;
+        }
+        if (item.type === StructureType.Select) {
+            this.model[name].items = item.items;
+        }
+    }
+}
+</script>
