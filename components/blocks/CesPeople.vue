d0207b5d7c3c5edad66ea7d780dd253247f874d1
alexisponcedev <poncealexis640@gmail.com>
2023-01-19 09:30:01 +0330

    tiny undo

---
+++ components/blocks/CesPeople.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <img src="/blocks/CesPeople.png" alt="" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
+import { StructureType } from "~/models/StructureType";
+import blockAddItem from "~/utils/blockAddItem";
+
+@Component
+export default class CesPeople extends Vue {
+    @Prop(Number) readonly id: number | undefined
+    @Prop({ default: true }) readonly editable: boolean | undefined
+    @VModel({ type: Object }) model!: any
+
+    mounted() {
+        blockAddItem(this.model, 'image1', { id: 1, type: StructureType.Image, title: "Image 1", src: '', alt: '' });
+        blockAddItem(this.model, 'image2', { id: 2, type: StructureType.Image, title: "Image 2", src: '', alt: '' });
+        blockAddItem(this.model, 'text1', { id: 5, type: StructureType.SimpleText, title: 'Text 1', value: '' });
+        blockAddItem(this.model, 'text2', { id: 6, type: StructureType.SimpleText, title: 'Text 2', value: '' });
+    }
+
+    get isEmpty(): Boolean {
+        return this.model && Object.keys(this.model).length === 0;
+    }
+}
+</script>
