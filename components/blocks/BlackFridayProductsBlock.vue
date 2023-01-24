aca6fcdb5bdd043af467fb980c02f6e0b319cdc0
alexisponcedev <poncealexis640@gmail.com>
2023-01-24 15:26:41 +0330

    update BlackFridayProductsBlock component

--- components/blocks/BlackFridayProductsBlock.vue
+++ components/blocks/BlackFridayProductsBlock.vue
@@ -1,6 +1,33 @@
 <template>
-    <div>
-        <img src="/blocks/BlackFridayProductsBlock.png" alt=""/>
+    <div class="pa-12">
+<!--        <img src="/blocks/BlackFridayProductsBlock.png" alt=""/>-->
+        <div class="products-container tw-space-y-2">
+            <div v-for="(product , index) in model.list.value" :key="index">
+                <div class="tw-flex tw-items-center tw-justify-between tw-bg-gray-50 tw-rounded-xl tw-p-2">
+                    <div class="tw-w-56">
+                        <img :src="product.image" alt="product image" class="tw-object-cover tw-h-16 tw-rounded-lg">
+                    </div>
+                    <div class="tw-flex-1">
+                        <div class="tw-text-gray-600">{{ product.title }}</div>
+                        <div class="tw-text-gray-600">{{ product.model }}</div>
+                    </div>
+                    <div class="tw-flex-1 tw-flex tw-items-center">
+                        <form-field-text :field="{ label: 'Price', placeholder: 'enter price', rules: [], colAttrs: {cols: 4}}" v-model="product.price"/>
+                        <form-field-text :field="{ label: 'Discount', placeholder: 'enter discount', rules: [], colAttrs: {cols: 4}}" v-model="product.discount"/>
+                    </div>
+                    <div class="tw-w-40">
+                        <div class="tw-font-bold tw-text-md">$ {{(product.price - (product.price * product.discount / 100)  ) }}</div>
+                    </div>
+                    <div class="tw-flex tw-items-center tw-space-x-4">
+                        <button @click="remove(index)">
+                            <v-icon small class="red--text">mdi-delete</v-icon>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <search-products @addProduct="addProduct" class=""/>
+
     </div>
 </template>
 
@@ -15,27 +42,32 @@ export default class BlackFridayProductsBlock extends Vue {
     @Prop({default: true}) readonly editable: boolean | undefined
     @VModel({type: Object}) model!: any
 
+    products  : Array<any> = []
+
+    addProduct(product : any){
+        this.model.list.value.push({
+            id : product.id,
+            model : product.model,
+            image : product.image,
+            title : product.name,
+            price : 0,
+            discount : 0,
+        });
+    }
+    remove(index: number) {
+        this.model.list.value.splice(index, 1);
+    }
     mounted() {
         blockAddItem(this.model, 'title', {id: 0, type: StructureType.SimpleText, title: 'Title', value: ''})
         blockAddItem(this.model, 'list', {
-            id: 1, type: StructureType.List, title: 'List', newItem: {
-                image: {id: 0, type: StructureType.Image, title: 'Product Image', src: '', alt: ''},
-                title: {id: 1, type: StructureType.String, title: 'Title', value: ''},
-                subtitle: {id: 1, type: StructureType.String, title: 'Subtitle', value: ''},
-                link: {id: 1, type: StructureType.Url, title: '', value: ''},
-            },
+            id: 1, type: StructureType.List, title: 'List', newItem: {},
             value: [
-                {
-                    image: {id: 0, type: StructureType.Image, title: 'Product Image', src: '', alt: ''},
-                    title: {id: 1, type: StructureType.String, title: 'Title', value: ''},
-                    subtitle: {id: 1, type: StructureType.String, title: 'Subtitle', value: ''},
-                    link: {id: 1, type: StructureType.Url, title: '', value: ''},
-                }
             ]
         });
         blockAddItem(this.model, 'text', {id: 2, type: StructureType.SimpleText, title: 'Bottom Text', value: ''})
 
 
+        this.model = {... this.model}
     }
 
     get isEmpty(): Boolean {
