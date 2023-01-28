8f0953eb718514d3df2f51a74458c5ca1c93d3f1
alexisponcedev <poncealexis640@gmail.com>
2023-01-28 15:13:02 +0330

    update blackFridayProductsBlock Component - add title to each row

--- components/blocks/BlackFridayProductsBlock.vue
+++ components/blocks/BlackFridayProductsBlock.vue
@@ -2,43 +2,48 @@
     <div class="pa-12" v-if="!isEmpty">
         <!--        <img src="/blocks/BlackFridayProductsBlock.png" alt=""/>-->
         <div class="products-container tw-space-y-2">
-            <div v-for="(product , index) in model.list.value" :key="index">
-                <div class="tw-flex tw-items-center tw-justify-between tw-bg-gray-50 tw-rounded-xl tw-p-2">
-                    <div class="tw-w-36">
-                        <img :src="product.image" alt="product image" class="tw-object-cover tw-h-16 tw-rounded-lg">
-                    </div>
-                    <div class="tw-w-64">
-                        <form-field-text class="tw-w-full"
-                                         :field="{ label: 'Title', placeholder: 'enter title', rules: [], colAttrs: {cols: 12}}"
-                                         v-model="product.title"/>
-                    </div>
-                    <div class="tw-flex-1">
-                        <div class="tw-flex tw-items-center" v-for="serie in product.series">
-                            <div class="tw-text-gray-600 tw-w-52 tw-text-right">{{ serie.title }}</div>
-                            <form-field-text class=""
-                                             :field="{ label: 'Price $', placeholder: 'enter price', rules: [], colAttrs: {cols: 2}}"
-                                             v-model="serie.price"/>
-                            <form-field-text
-                                @input="serie.final_price = (serie.price - (serie.price * serie.discount / 100))"
-                                :field="{ label: 'Discount %', placeholder: 'enter discount', rules: [], colAttrs: {cols: 2}}"
-                                v-model="serie.discount"/>
+            <draggable v-model="model.list.value" group="product-list-black-friday">
+                <div v-for="(product , index) in model.list.value" :key="index">
+                    <div class="tw-flex tw-items-center tw-justify-between tw-bg-gray-50 tw-rounded-xl tw-p-2">
+                        <div class="tw-w-36">
+                            <img :src="product.image" alt="product image" class="tw-object-cover tw-h-16 tw-rounded-lg">
+                        </div>
+<!--                        <div class="tw-w-64">-->
+<!--                            <form-field-text class="tw-w-full"-->
+<!--                                             :field="{ label: 'Title', placeholder: 'enter title', rules: [], colAttrs: {cols: 12}}"-->
+<!--                                             v-model="product.title"/>-->
+<!--                        </div>-->
+                        <div class="tw-flex-1">
+                            <div class="tw-flex tw-items-center" v-for="(serie , index) in product.series">
+                                <div class="tw-text-gray-600 tw-w-52 tw-text-right">{{ serie.title }}</div>
+                                <form-field-text class=""
+                                                 :field="{ label: 'Title ' + (index + 1), placeholder: '', rules: [], colAttrs: {cols: 4}}"
+                                                 v-model="serie.header"/>
+                                <form-field-text class=""
+                                                 :field="{ label: 'Price $', placeholder: 'enter price', rules: [], colAttrs: {cols: 2}}"
+                                                 v-model="serie.price"/>
+                                <form-field-text
+                                    @input="serie.final_price = (serie.price - (serie.price * serie.discount / 100))"
+                                    :field="{ label: 'Discount %', placeholder: 'enter discount', rules: [], colAttrs: {cols: 2}}"
+                                    v-model="serie.discount"/>
 
-                            <form-field-text
-                                :field="{ label: 'Final Price $', placeholder: 'enter FinalPrice', rules: [], colAttrs: {cols: 2}}"
-                                v-model="serie.final_price"/>
+                                <form-field-text
+                                    :field="{ label: 'Final Price $', placeholder: 'enter FinalPrice', rules: [], colAttrs: {cols: 2}}"
+                                    v-model="serie.final_price"/>
 
-<!--                            <div class="tw-font-bold tw-text-md">$-->
-<!--                                {{ (serie.price - (serie.price * serie.discount / 100)) }}-->
-<!--                            </div>-->
+                                <!--                            <div class="tw-font-bold tw-text-md">$-->
+                                <!--                                {{ (serie.price - (serie.price * serie.discount / 100)) }}-->
+                                <!--                            </div>-->
+                            </div>
+                        </div>
+                        <div class="tw-flex tw-items-center tw-space-x-4">
+                            <button @click="remove(index)">
+                                <v-icon small class="red--text">mdi-delete</v-icon>
+                            </button>
                         </div>
                     </div>
-                    <div class="tw-flex tw-items-center tw-space-x-4">
-                        <button @click="remove(index)">
-                            <v-icon small class="red--text">mdi-delete</v-icon>
-                        </button>
-                    </div>
                 </div>
-            </div>
+            </draggable>
         </div>
         <search-products @addProduct="addProduct" class=""/>
 
@@ -65,12 +70,12 @@ export default class BlackFridayProductsBlock extends Vue {
             id: product.id,
             model: product.model,
             image: product.image,
-            title: '', //product.name,
+            // title: '', //product.name,
             series: product.productSeries.length > 0 ?
                 product.productSeries[0].values.map((i: any) => {
-                    return {title: i.title, id: i.products[0], price: 0, discount: 0, final_price: 0}
+                    return {header: '', title: i.title, id: i.products[0], price: 0, discount: 0, final_price: 0}
                 }) :
-                [{title: product.name, id: product.id, price: 0, discount: 0, final_price: 0}]
+                [{header: product.name, title: product.name, id: product.id, price: 0, discount: 0, final_price: 0}]
             ,
             price: 0,
             discount: 0,
