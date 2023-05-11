3d229b896c09411f532c9c6f4cac95dc780118d4
alexisponcedev <poncealexis640@gmail.com>
2023-05-11 16:10:16 +0330

    add nasim first batch of components

---
+++ components/blocks/PressArticleSlider.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <img src="/blocks/PressArticleSlider.png" alt="" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
+import { StructureType } from "~/models/StructureType";
+import blockAddItem from "~/utils/blockAddItem";
+
+@Component
+export default class PressArticleSlider extends Vue {
+    @Prop(Number) readonly id: number | undefined;
+    @Prop({ default: true }) readonly editable: Boolean | undefined;
+    @VModel({ type: Object }) model!: any;
+
+    mounted() {
+        blockAddItem(this.model, 'title', {
+            id: 0,
+            type: StructureType.String,
+            title: 'Title',
+            value: 'Press Article Slider'
+        });
+        blockAddItem(this.model, "list", {
+            id: 1,
+            type: StructureType.List,
+            title: "List",
+            newItem: {
+                subtitle: {
+                    id: 1,
+                    type: StructureType.String,
+                    title: "Subtitle",
+                    value: "",
+                },
+                image: {
+                    id: 2,
+                    type: StructureType.Image,
+                    title: "Image",
+                    src: "",
+                    alt: "Image Alt",
+                },
+                EnlargeBtn: {
+                    id: 3,
+                    type: StructureType.Url,
+                    title: "Enlarge Image",
+                    value: "",
+                },
+                downloadBtn: {
+                    id: 4,
+                    type: StructureType.Url,
+                    title: "Download Image",
+                    value: "",
+                },
+            },
+            value: [],
+        });
+        this.model = { ...this.model };
+    }
+
+    get isEmpty(): Boolean {
+        return this.model && Object.keys(this.model).length === 0;
+    }
+}
+</script>
