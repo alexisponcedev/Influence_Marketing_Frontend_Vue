e116445393c083653071b8c33eb3cb11e061c56b
alexisponcedev <poncealexis640@gmail.com>
2023-06-15 11:13:30 +0330

    template add to uledx

--- components/blocks/ULEDXFullPageComponent.vue
+++ components/blocks/ULEDXFullPageComponent.vue
@@ -6,6 +6,8 @@
 
 <script lang="ts">
 import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
+import { StructureType } from "~/models/StructureType";
+import blockAddItem from "~/utils/blockAddItem";
 
 @Component
 export default class ULEDXFullPageComponent extends Vue {
@@ -14,6 +16,16 @@ export default class ULEDXFullPageComponent extends Vue {
     @VModel({ type: Object }) model!: any;
 
     mounted() {
+        blockAddItem(this.model, "template", {
+            id: 0,
+            type: StructureType.Select,
+            title: "Template",
+            value: "v1",
+            items: [
+                { title: "V1", value: "v1" },
+                { title: "V2", value: "v2"},
+            ],
+        });
         this.model = { ...this.model };
     }
 
