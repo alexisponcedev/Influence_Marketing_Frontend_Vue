ce72a89c62a1872af85130b15c63a2d0243aae70
alexisponcedev <poncealexis640@gmail.com>
2023-06-20 16:20:14 +0330

    new componet

---
+++ components/blocks/CareersTextBox.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <img src="/blocks/CareersTextBox.png" alt="" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
+import { StructureType } from "~/models/StructureType";
+import blockAddItem from "~/utils/blockAddItem";
+
+@Component
+export default class CareersTextBox extends Vue {
+    @Prop(Number) readonly id: number | undefined;
+    @Prop({ default: true }) readonly editable: boolean | undefined;
+    @VModel({ type: Object }) model!: any;
+
+    mounted() {
+        blockAddItem(this.model, "backgroundColor", {
+            id: 0,
+            type: StructureType.Color,
+            title: "Background Color",
+            value: "",
+        });
+        blockAddItem(this.model, "image", {
+            id: 0,
+            type: StructureType.Image,
+            title: "Logo",
+            src: "",
+            alt: "",
+        });
+        blockAddItem(this.model, "text", {
+            id: 1,
+            type: StructureType.SimpleText,
+            title: "Title",
+            value: "",
+        });
+    }
+
+    get isEmpty(): Boolean {
+        return this.model && Object.keys(this.model).length === 0;
+    }
+}
+</script>
