e42894509c698d3730d28b6a9b699b7b4cd71664
alexisponcedev <poncealexis640@gmail.com>
2023-06-22 16:47:16 +0330

    exclude id added to NewsPressArchive component

--- components/blocks/NewsPressArchive.vue
+++ components/blocks/NewsPressArchive.vue
@@ -6,9 +6,9 @@
 
 <script lang="ts">
 import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
-import { StructureType } from "~/models/StructureType";
+import { StructureType } from "@/models/StructureType";
+import blockAddItem from "@/utils/blockAddItem";
 import { Api } from "@/store";
-import blockAddItem from "~/utils/blockAddItem";
 
 @Component
 export default class NewsPressArchive extends Vue {
@@ -19,6 +19,7 @@ export default class NewsPressArchive extends Vue {
     Api = Api;
 
     async mounted() {
+        await Api.Post.getAllNews();
         blockAddItem(this.model, "titleOne", {
             id: 1,
             type: StructureType.String,
@@ -55,117 +56,115 @@ export default class NewsPressArchive extends Vue {
             title: "Number Of latest news post to get",
             value: 4,
         });
-
-        // blockAddItem(this.model, "list", {
-        //     id: 4,
-        //     type: StructureType.List,
-        //     title: "List",
-        //     newItem: {
-        //         image: {
-        //             id: 0,
-        //             type: StructureType.Image,
-        //             title: "Image",
-        //             src: "",
-        //             alt: "",
-        //         },
-        //         title: {
-        //             id: 1,
-        //             type: StructureType.String,
-        //             title: "Title",
-        //             value: "",
-        //         },
-        //         tagLink: {
-        //             id: 2,
-        //             type: StructureType.Url,
-        //             target: "_self",
-        //             title: "Tag Link",
-        //             value: "",
-        //         },
-        //         link: {
-        //             id: 3,
-        //             type: StructureType.Url,
-        //             target: "_self",
-        //             title: "Link",
-        //             value: "",
-        //         },
-        //     },
-        //     value: [],
-        // });
-
-        // await Api.Post.getAllNews();
-        // this.addPosts();
+        blockAddItem(this.model, "selectNews", {
+            id: 7,
+            type: StructureType.AutoCompeleteSelect,
+            title: "Exclude News",
+            itemText: "page.title",
+            itemValue: "id",
+            value: "",
+            items: () =>
+                Api.Post.allNews
+                    .filter((p: any) => p.hasOwnProperty("page") && p.page)
+                    .sort((a: any, b: any) => (a.id < b.id ? 1 : -1)),
+        });
+        blockAddItem(this.model, "exclude_news", {
+            id: 8,
+            type: StructureType.List,
+            title: "Excluded News List",
+            maxLength: 0,
+            newItem: {
+                image: {
+                    id: 0,
+                    type: StructureType.Image,
+                    title: "Image",
+                    src: "",
+                    alt: "",
+                },
+                title: {
+                    id: 1,
+                    type: StructureType.String,
+                    title: "Title",
+                    value: "",
+                },
+                tag: {
+                    id: 2,
+                    type: StructureType.String,
+                    title: "Tag",
+                    value: "",
+                },
+                link: {
+                    id: 3,
+                    type: StructureType.Url,
+                    target: "_self",
+                    title: "Link",
+                    value: "",
+                },
+            },
+            value: [],
+        });
 
         this.model = { ...this.model };
     }
 
-    // addPosts() {
-    //     this.model.list.value = [];
-    //     let posts = Api.Post.allNews
-    //         .filter((p: any) => p.hasOwnProperty("page") && p.page)
-    //         .sort((a: any, b: any) => (a.id < b.id ? 1 : -1));
-    //
-    //     for (let i = 0; i < this.model.count.value; i++) {
-    //         this.model.list.value.push(this.addPost(posts[i]));
-    //     }
-    // }
+    get selectNews() {
+        return this.model.hasOwnProperty("selectNews")
+            ? this.model.selectNews.value
+            : null;
+    }
 
-    // get count() {
-    //     return this.model.hasOwnProperty("count") ? this.model.count.value : 0;
-    // }
+    @Watch("selectNews")
+    onSelectNews(value: any) {
+        if (value) {
+            this.model.exclude_news.value.push(
+                this.addPost(Api.Post.allNews.find((item) => item.id == value))
+            );
+            this.model.selectNews.value = "";
+        }
+    }
 
-    // @Watch("count")
-    // onCountChanged() {
-    //     this.addPosts();
-    // }
-    //
-    // addPost(post: any, title: string = "Small Title"): any {
-    //     let newItem: any = {
-    //         id: 1,
-    //         type: StructureType.Object,
-    //         title: title,
-    //         value: {
-    //             image: {
-    //                 id: 0,
-    //                 type: StructureType.Image,
-    //                 title: "Image",
-    //                 src: "",
-    //                 alt: "",
-    //             },
-    //             title: {
-    //                 id: 1,
-    //                 type: StructureType.String,
-    //                 title: "Title",
-    //                 value: "",
-    //             },
-    //             tagLink: {
-    //                 id: 2,
-    //                 type: StructureType.Url,
-    //                 target: "_self",
-    //                 title: "Tag Link",
-    //                 value: "",
-    //             },
-    //             link: {
-    //                 id: 3,
-    //                 type: StructureType.Url,
-    //                 target: "_self",
-    //                 title: "READ ARTICLE",
-    //                 value: "",
-    //             },
-    //         },
-    //     };
-    //     if (post) {
-    //         let imageMeta = post.page.meta.find(
-    //             (i: any) => i.name.includes("og:image") && i.content != null
-    //         );
-    //         if (imageMeta) newItem.value.image.src = imageMeta.content;
-    //         newItem.value.title.value = post.page.title;
-    //         newItem.value.link.value = post.page.route;
-    //     }
-    //     return newItem;
-    // }
-
-    get isEmpty(): Boolean {
-        return this.model && Object.keys(this.model).length === 0;
+    addPost(post: any): any {
+        let newItem: any = {
+            image: {
+                id: 0,
+                type: StructureType.Image,
+                title: "Image",
+                src: "",
+                alt: "",
+            },
+            title: {
+                id: 1,
+                type: StructureType.String,
+                title: "Title",
+                value: "",
+            },
+            tag: {
+                id: 2,
+                type: StructureType.String,
+                target: "_self",
+                title: "Tag",
+                value: "",
+            },
+            link: {
+                id: 3,
+                type: StructureType.Url,
+                target: "_self",
+                title: "READ ARTICLE",
+                value: "",
+            },
+        };
+        if (post) {
+            let imageMeta = post.page.meta.find(
+                (i: any) => i.name.includes("og:image") && i.content != null
+            );
+            if (imageMeta) newItem.image.src = imageMeta.content;
+            newItem.title.value = post.page.title;
+            newItem.link.value = post.page.route;
+            newItem.tag.value = post.tags.join(", ");
+            newItem.created_at = post.page.created_at;
+            newItem.id = post.page.id;
+        }
+        return newItem;
     }
 }
 </script>
