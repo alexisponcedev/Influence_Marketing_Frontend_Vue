98ceb0671652683431c81bfa5c4f7733267809fa
alexisponcedev <poncealexis640@gmail.com>
2023-07-30 12:48:05 +0330

    add sub category to product grid component

--- components/structure/editor/index.vue
+++ components/structure/editor/index.vue
@@ -1,45 +1,51 @@
 <template>
     <div class="">
-        <div class="tw-flex tw-items-center tw-w-full tw-justify-between tw-p-3" v-if="!simple">
+        <div
+            class="tw-flex tw-items-center tw-w-full tw-justify-between tw-p-3"
+            v-if="!simple"
+        >
             <h6>{{ title }}</h6>
             <button @click="close">
                 <v-icon>mdi-close</v-icon>
             </button>
         </div>
 
-        <component :is="`structure-editor-${structure[key].type}`"
-                   v-for="(key , index) in structureKeys.filter(i => i.type !== null)"
-                   v-model="structure[key]"
-                   :key="index"/>
-
+        <component
+            :is="`structure-editor-${structure[key].type}`"
+            v-for="(key, index) in structureKeys.filter((i) => i.type !== null)"
+            v-model="structure[key]"
+            :key="index"
+        />
     </div>
 </template>
 
 <script lang="ts">
-import {Vue, Component, Prop, VModel, Emit} from "vue-property-decorator";
-import {StructureField} from "~/interfaces/StructureField";
+import { Vue, Component, Prop, VModel, Emit } from "vue-property-decorator";
+import { StructureField } from "@/interfaces/StructureField";
 
 @Component
 export default class StructureEditor extends Vue {
-    @Prop(String) readonly title: string | undefined
-    @Prop({type: Boolean, default: false}) readonly simple: boolean | undefined
-    @VModel({type: StructureField}) structure!: StructureField | any
+    @Prop(String) readonly title: string | undefined;
+    @Prop({ type: Boolean, default: false }) readonly simple:
+        | boolean
+        | undefined;
+    @VModel() structure!: StructureField | any;
 
-    @Emit('close')
+    @Emit("close")
     close() {
-        this.$emit('close');
+        this.$emit("close");
     }
 
     get structureKeys() {
-
-        return this.structure ?
-            // Object.keys(this.structure)
-            Object.entries(this.structure)
-                .sort((a: any, b: any) => {
-                    if (a[1].id < b[1].id) return -1; else if (a[1].id > b[1].id) return 1;
-                    return 0
-                })
-                .map(i => i[0])
+        return this.structure
+            ? // Object.keys(this.structure)
+              Object.entries(this.structure)
+                  .sort((a: any, b: any) => {
+                      if (a[1].id < b[1].id) return -1;
+                      else if (a[1].id > b[1].id) return 1;
+                      return 0;
+                  })
+                  .map((i) => i[0])
             : [];
     }
 }
--- components/structure/editor/select.vue
+++ components/structure/editor/select.vue
@@ -1,31 +1,33 @@
 <template>
-    <form-field-select
-        :field="field"
-        v-model="model.value"/>
+    <form-field-select :field="field" v-model="model.value" @change="changed" />
 </template>
 
 <script lang="ts">
-import {Vue, Component, Prop, Watch, VModel} from "vue-property-decorator";
-import {StructureField} from "~/interfaces/StructureField";
+import { Vue, Component, VModel } from "vue-property-decorator";
+import { StructureField } from "@/interfaces/StructureField";
 
 @Component
 export default class StructureSelectEditor extends Vue {
-    @VModel({type: StructureField}) model!: StructureField
+    @VModel() model!: StructureField;
 
     field = {
-        label: 'Filed',
+        label: "Filed",
         rules: [],
-        'item-text': 'title',
-        'item-value': 'value',
-        colAttrs: {cols: 12},
-        items: []
-    }
+        "item-text": "title",
+        "item-value": "value",
+        colAttrs: { cols: 12 },
+        items: [],
+    };
 
     mounted() {
-        this.field.label = this.model.title ?? 'field';
-        this.field["item-text"] = this.model.itemText ?? 'title';
-        this.field["item-value"] = this.model.itemValue ?? 'value';
+        this.field.label = this.model.title ?? "field";
+        this.field["item-text"] = this.model.itemText ?? "title";
+        this.field["item-value"] = this.model.itemValue ?? "value";
         this.field.items = this.model.items;
+    }
+
+    changed(event: any) {
+        if (this.model && this.model.onChanged) this.model.onChanged(event);
     }
 }
 </script>
