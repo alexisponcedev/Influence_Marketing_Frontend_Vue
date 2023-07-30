e45c94b2af983681d231565ac6d8d5f73b015ab2
alexisponcedev <poncealexis640@gmail.com>
2023-07-30 13:17:39 +0330

    add textColor to LetsGetRealTextBox component

--- components/blocks/LetsGetRealTextBox.vue
+++ components/blocks/LetsGetRealTextBox.vue
@@ -5,18 +5,14 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
-import { StructureType } from "~/models/StructureType";
-import blockAddItem from "~/utils/blockAddItem";
-import { Theme } from "~/interfaces/ThemeEnum";
+import { Vue, Component, VModel } from "vue-property-decorator";
+import { StructureType } from "@/models/StructureType";
+import blockAddItem from "@/utils/blockAddItem";
+import { Theme } from "@/interfaces/ThemeEnum";
 
 @Component
 export default class BlockLetsGetRealTextBox extends Vue {
-    @Prop(Number) readonly id: number | undefined;
-    @Prop({ default: true }) readonly editable: Boolean | undefined;
     @VModel({ type: Object }) model!: any;
-
-    Theme = Theme;
 
     mounted() {
         blockAddItem(this.model, "template", {
@@ -29,28 +25,33 @@ export default class BlockLetsGetRealTextBox extends Vue {
                 { title: "V2", value: "v2" },
             ],
         });
+        
         blockAddItem(this.model, "theme", {
             id: 1,
             type: StructureType.Select,
             title: "Theme",
             value: Theme.dark,
             items: [
-                { title: "Light", value: this.Theme.light },
-                { title: "Dark", value: this.Theme.dark },
+                { title: "Light", value: Theme.light },
+                { title: "Dark", value: Theme.dark },
             ],
         });
+
         blockAddItem(this.model, "title", {
             id: 2,
             type: StructureType.Text,
             title: "Title",
             value: '<h2 class="ql-align-center">Let\'s Get real.</h2><h2 class="ql-align-center">Youre here because </h2><h2 class="ql-align-center">you want a new TV.</h2>',
         });
-    }
 
-    get isEmpty(): Boolean {
-        return this.model && Object.keys(this.model).length === 0;
+        blockAddItem(this.model, "textColor", {
+            id: 3,
+            type: StructureType.Color,
+            title: "Text Color",
+            value: "#8C8F8F",
+        });
+
+        this.model = { ...this.model };
     }
 }
 </script>
-
-<style type="text/css"></style>
