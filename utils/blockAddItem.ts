e1d4cbeda1c8b18add5cc559acddc6e8ccc308a3
alexisponcedev <poncealexis640@gmail.com>
2023-08-17 14:29:31 +0330

    some missing updates

--- utils/blockAddItem.ts
+++ utils/blockAddItem.ts
@@ -1,12 +1,10 @@
-import {StructureType} from "~/models/StructureType";
+import { StructureType } from "@/models/StructureType";
 
 export default function blockAddItem(base: any, name: string, item: any) {
-
     if (!base.hasOwnProperty(name)) base[name] = item;
 
     base[name].id = item.id;
     // base[name].title = item.title;
-
 
     if (base[name].type !== item.type) base[name].type = item.type;
 
@@ -23,19 +21,24 @@ export default function blockAddItem(base: any, name: string, item: any) {
         base[name].newItem = item.newItem;
         base[name].value.forEach((element: any, index: number) => {
             Object.keys(item.newItem).forEach((key: string) => {
-                blockAddItem(base[name].value[index], key, JSON.parse(JSON.stringify(item.newItem[key])))
-            })
-        })
-        base[name].hidden = item.hidden || false
+                blockAddItem(
+                    base[name].value[index],
+                    key,
+                    JSON.parse(JSON.stringify(item.newItem[key]))
+                );
+            });
+        });
+        base[name].hidden = item.hidden || false;
     }
 
     if (item.type === StructureType.Object) {
         Object.keys(item.value).forEach((key: string) => {
-            blockAddItem(base[name].value, key, item.value[key])
-        })
+            blockAddItem(base[name].value, key, item.value[key]);
+        });
     }
 
     if (item.type === StructureType.Select) {
         base[name].items = item.items;
+        base[name].disabled = item.disabled;
     }
 }
