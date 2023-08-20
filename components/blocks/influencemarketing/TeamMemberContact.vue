022c89d9f85b92a3bbf78683f9cc4f14c3e7bb43
alexisponcedev <poncealexis640@gmail.com>
2023-08-20 15:43:46 +0330

    added color

--- components/blocks/influencemarketing/TeamMemberContact.vue
+++ components/blocks/influencemarketing/TeamMemberContact.vue
@@ -72,6 +72,12 @@ export default class TeamMemberContact extends Vue {
             title: "title",
             value: "",
         });
+        blockAddItem(this.model, "title", {
+            id: 2,
+            type: StructureType.Color,
+            title: "Background Color",
+            value: "#B6DCE4",
+        });
         this.model = { ...this.model };
     }
 
