1edbff8eca70bcb1e3b0159841f3350d7331094e
alexisponcedev <poncealexis640@gmail.com>
2023-08-27 14:32:54 +0330

    remove unnecessary components in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,23 @@ echo "BASE_API_PATH=$BASE_API_PATH"
 
 sed -i "s#export const BASE_PATH = \".*\".replace#export const BASE_PATH = \"$BASE_API_PATH\".replace#" ./repositories/base.ts
 
+# remove unnecessary components:
+if [ -z ${BRANDS_NAME+x} ]; then
+	echo "BRANDS_NAME is unset";
+else
+	value=`cat ./components/blocks/brands.txt`
+	IFS=', ' read -r -a brands <<< "$value"
+	IFS=', ' read -r -a selected_brands <<< "$BRANDS_NAME"
+	for element in "${selected_brands[@]}"; do
+		brands=(${brands[@]/*${element}*/})
+	done
+
+	for element in "${brands[@]}"
+	do
+		rm -rf "./components/blocks/$element"
+	done
+fi
+
 yarn install
 # yarn build
 yarn generate
