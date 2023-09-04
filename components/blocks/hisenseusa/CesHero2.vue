3ff6fe47aa7f18fb786cf984dbf4661ea115d070
alexisponcedev <poncealexis640@gmail.com>
2023-09-04 11:20:56 +0330

    ces versioning

--- components/blocks/hisenseusa/CesHero2.vue
+++ components/blocks/hisenseusa/CesHero2.vue
@@ -9,15 +9,16 @@ import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
 import { StructureType } from "~/models/StructureType";
 import blockAddItem from "~/utils/blockAddItem";
 import { Theme } from "~/interfaces/ThemeEnum";
+import blockRemoveItem from "~/utils/blockRemoveItem";
 
 @Component
 export default class CesHero2 extends Vue {
-    @Prop(Number) readonly id: number | undefined
-    @Prop({ default: true }) readonly editable: boolean | undefined
-    @VModel({ type: Object }) model!: any
+    @Prop(Number) readonly id: number | undefined;
+    @Prop({ default: true }) readonly editable: boolean | undefined;
+    @VModel({ type: Object }) model!: any;
 
-    mounted() {
-        blockAddItem(this.model, 'theme', {
+    prepare() {
+        blockAddItem(this.model, "theme", {
             id: 0,
             type: StructureType.Select,
             title: "Theme",
@@ -27,13 +28,68 @@ export default class CesHero2 extends Vue {
                 { title: "Dark", value: Theme.dark },
             ],
         });
-        blockAddItem(this.model, 'backgroundImage', {
+        blockAddItem(this.model, "backgroundImage", {
             id: 2,
             type: StructureType.Image,
-            title: 'Background Image',
-            src: '',
-            alt: ''
+            title: "Background Image",
+            src: "",
+            alt: "",
         });
+
+        if (this.model.template.value === "v2") {
+            blockAddItem(this.model, "title", {
+                id: 4,
+                type: StructureType.String,
+                title: "Title",
+                value: "",
+            });
+            blockAddItem(this.model, "text", {
+                id: 5,
+                type: StructureType.SimpleText,
+                title: "Text",
+                value: "",
+            });
+            blockAddItem(this.model, "link", {
+                id: 6,
+                type: StructureType.Url,
+                title: "Link",
+                value: "",
+            });
+            blockAddItem(this.model, "image", {
+                id: 7,
+                type: StructureType.Image,
+                title: "Image",
+                src: "",
+                alt: "",
+            });
+        } else blockRemoveItem(this.model, ["title", "text", "link", "image"]);
+
+        this.model = { ...this.model };
+    }
+
+    mounted() {
+        blockAddItem(this.model, "template", {
+            id: 0,
+            type: StructureType.Select,
+            title: "Template",
+            value: "v1",
+            items: [
+                { title: "Version 1", value: "v1" },
+                { title: "Version 2", value: "v2" },
+            ],
+        });
+        this.prepare();
+    }
+
+    get template() {
+        return this.model && this.model.template
+            ? this.model.template.value
+            : "";
+    }
+
+    @Watch("template")
+    onTemplateChanged() {
+        this.prepare();
     }
 
     get isEmpty(): Boolean {
