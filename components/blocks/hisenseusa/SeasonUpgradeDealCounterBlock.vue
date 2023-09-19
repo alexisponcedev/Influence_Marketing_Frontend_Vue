f983bb1940bec615c7acbac61a8f667939591765
alexisponcedev <poncealexis640@gmail.com>
2023-09-19 19:04:02 +0330

    added season upgrade structures

---
+++ components/blocks/hisenseusa/SeasonUpgradeDealCounterBlock.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <img src="/blocks/SeasonUpgradeDealCounterBlock.png" alt="" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
+import { StructureType } from "~/models/StructureType";
+import { Theme } from "~/interfaces/ThemeEnum";
+import blockAddItem from "~/utils/blockAddItem";
+
+@Component
+export default class SeasonUpgradeDealCounterBlock extends Vue {
+    @Prop(Number) readonly id: number | undefined;
+    @Prop(Number) readonly product_id!: number;
+    @VModel({ type: Object }) model!: any;
+
+    Theme = Theme;
+
+    mounted() {
+        blockAddItem(this.model, "list", {
+            id: 4,
+            type: StructureType.List,
+            title: "Week List",
+            newItem: {
+                object: {
+                    id: 1,
+                    type: StructureType.Object,
+                    title: "Week Item",
+                    value: {
+                        title: {
+                            id: 1,
+                            type: StructureType.String,
+                            title: "Title",
+                            value: "",
+                        },
+                        status: {
+                            id: 2,
+                            type: StructureType.Select,
+                            title: "Status",
+                            value: "deactive",
+                            items: [
+                                { title: "Deactive", value: "deactive" },
+                                { title: "Active", value: "active" },
+                            ],
+                        },
+                    },
+                },
+            },
+            value: [
+                {
+                    object: {
+                        id: 1,
+                        type: StructureType.Object,
+                        title: "Week Item",
+                        value: {
+                            title: {
+                                id: 1,
+                                type: StructureType.String,
+                                title: "Title",
+                                value: "",
+                            },
+                            status: {
+                                id: 2,
+                                type: StructureType.Select,
+                                title: "Status",
+                                value: "deactive",
+                                items: [
+                                    { title: "Deactive", value: "deactive" },
+                                    { title: "Active", value: "active" },
+                                ],
+                            },
+                        },
+                    },
+                },
+            ],
+        });
+
+        this.model = { ...this.model };
+    }
+
+    get isEmpty(): Boolean {
+        return this.model && Object.keys(this.model).length === 0;
+    }
+}
+</script>
+
+<style type="text/css"></style>
---
+++ components/blocks/hisenseusa/SeasonUpgradeProductsCarousel.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <img src="/blocks/SeasonUpgradeProductsCarousel.png" alt="" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
+import { StructureType } from "~/models/StructureType";
+import { Theme } from "~/interfaces/ThemeEnum";
+import blockAddItem from "~/utils/blockAddItem";
+
+@Component
+export default class SeasonUpgradeProductsCarousel extends Vue {
+    @Prop(Number) readonly id: number | undefined;
+    @Prop(Number) readonly product_id!: number;
+    @VModel({ type: Object }) model!: any;
+
+    Theme = Theme;
+
+    mounted() {
+        blockAddItem(this.model, "subtitle", {
+            id: 0,
+            type: StructureType.String,
+            title: "Subtitle",
+            value: "",
+        });
+        blockAddItem(this.model, "title", {
+            id: 1,
+            type: StructureType.SimpleText,
+            title: "Title",
+            value: "",
+        });
+        blockAddItem(this.model, "text", {
+            id: 2,
+            type: StructureType.Select,
+            title: "Theme",
+            value: "light",
+            items: [
+                { title: "Light", value: "light" },
+                { title: "Gradient", value: "gradient" },
+            ],
+        });
+        blockAddItem(this.model, "direction", {
+            id: 3,
+            type: StructureType.Select,
+            title: "Carousel Start Direction",
+            value: "left",
+            items: [
+                { title: "Left", value: "left" },
+                { title: "Right", value: "right" },
+            ],
+        });
+
+        this.model = { ...this.model };
+    }
+
+    get isEmpty(): Boolean {
+        return this.model && Object.keys(this.model).length === 0;
+    }
+}
+</script>
+
+<style type="text/css"></style>
