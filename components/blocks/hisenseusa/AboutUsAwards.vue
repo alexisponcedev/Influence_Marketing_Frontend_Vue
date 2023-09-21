dace6981baaa73093e709374a880ed2baa3e383e
alexisponcedev <poncealexis640@gmail.com>
2023-09-21 13:31:03 +0330

    about us component implemented

---
+++ components/blocks/hisenseusa/AboutUsAwards.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <img src="/blocks/AboutUsAwards.png" alt="" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, VModel } from "vue-property-decorator";
+import { StructureType } from "@/models/StructureType";
+import blockAddItem from "@/utils/blockAddItem";
+
+@Component
+export default class AboutUsAwards extends Vue {
+    @VModel({ type: Object }) model!: any;
+
+    mounted() {
+        blockAddItem(this.model, "title", {
+            id: 1,
+            type: StructureType.SimpleText,
+            title: "Title",
+            value: "",
+        });
+
+        blockAddItem(this.model, "text", {
+            id: 2,
+            type: StructureType.SimpleText,
+            title: "Text",
+            value: "",
+        });
+
+        blockAddItem(this.model, "list", {
+            id: 3,
+            type: StructureType.List,
+            title: "List",
+            newItem: {
+                title: {
+                    id: 1,
+                    type: StructureType.SimpleText,
+                    title: "Title",
+                    value: "",
+                },
+                text: {
+                    id: 2,
+                    type: StructureType.SimpleText,
+                    title: "Text",
+                    value: "",
+                },
+            },
+            value: [],
+        });
+
+        this.model = { ...this.model };
+    }
+}
+</script>
---
+++ components/blocks/hisenseusa/AboutUsTimeline.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <img src="/blocks/AboutUsTimeline.png" alt="" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, VModel } from "vue-property-decorator";
+import { StructureType } from "@/models/StructureType";
+import blockAddItem from "@/utils/blockAddItem";
+
+@Component
+export default class AboutUsTimeline extends Vue {
+    @VModel({ type: Object }) model!: any;
+
+    mounted() {
+        blockAddItem(this.model, "list", {
+            id: 1,
+            type: StructureType.List,
+            title: "List",
+            newItem: {
+                title: {
+                    id: 1,
+                    type: StructureType.SimpleText,
+                    title: "Title",
+                    value: "",
+                },
+                list: {
+                    id: 2,
+                    type: StructureType.List,
+                    title: "List",
+                    newItem: {
+                        description: {
+                            id: 1,
+                            type: StructureType.SimpleText,
+                            title: "Description",
+                            value: "",
+                        },
+                        image: {
+                            id: 2,
+                            type: StructureType.Image,
+                            title: "Image",
+                            src: "",
+                            alt: "",
+                        },
+                    },
+                    value: [],
+                },
+            },
+            value: [],
+        });
+
+        this.model = { ...this.model };
+    }
+}
+</script>
