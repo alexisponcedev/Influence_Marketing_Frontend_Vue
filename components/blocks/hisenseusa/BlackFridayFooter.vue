6eb5aebafe9ed382a0a6cdd04193ea198cad22a4
alexisponcedev <poncealexis640@gmail.com>
2023-10-05 10:46:28 +0330

    updated black friday component

--- components/blocks/hisenseusa/BlackFridayFooter.vue
+++ components/blocks/hisenseusa/BlackFridayFooter.vue
@@ -8,6 +8,7 @@
 import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
 import { StructureType } from "~/models/StructureType";
 import blockAddItem from "~/utils/blockAddItem";
+import blockRemoveItem from "~/utils/blockRemoveItem";
 
 @Component
 export default class BlackFridayFooter extends Vue {
@@ -15,9 +16,9 @@ export default class BlackFridayFooter extends Vue {
     @Prop({ default: true }) readonly editable: boolean | undefined;
     @VModel({ type: Object }) model!: any;
 
-    mounted() {
+    prepare() {
         blockAddItem(this.model, "list", {
-            id: 0,
+            id: 1,
             type: StructureType.List,
             title: "List",
             newItem: {
@@ -42,11 +43,65 @@ export default class BlackFridayFooter extends Vue {
             ],
         });
         blockAddItem(this.model, "rightText", {
-            id: 1,
+            id: 2,
             type: StructureType.String,
             title: "Title",
             value: "",
         });
+        if (this.model.template.value === "v2") {
+            blockAddItem(this.model, "text_color", {
+                id: 3,
+                type: StructureType.Color,
+                title: "Text Color",
+                value: "#999999",
+            });
+            blockAddItem(this.model, "background_color", {
+                id: 4,
+                type: StructureType.Color,
+                title: "Background Color",
+                value: "#1a2a2d",
+            });
+            blockAddItem(this.model, "hover", {
+                id: 5,
+                type: StructureType.Select,
+                title: "Hover Effect",
+                value: "active",
+                items: [
+                    { title: "Active", value: "active" },
+                    { title: "Deactive", value: "deactive" },
+                ],
+            });
+        } else
+            blockRemoveItem(this.model, [
+                "text_color",
+                "background_color",
+                "hover",
+            ]);
+    }
+
+    mounted() {
+        blockAddItem(this.model, "template", {
+            id: 0,
+            type: StructureType.Select,
+            title: "Template",
+            value: "v1",
+            items: [
+                { title: "Version 1", value: "v1" },
+                { title: "Version 2", value: "v2" },
+            ],
+        });
+        this.prepare();
+    }
+
+    get template() {
+        return this.model && this.model.template
+            ? this.model.template.value
+            : "";
+    }
+
+    @Watch("template")
+    onTemplateChanged() {
+        this.prepare();
     }
 
     get isEmpty(): Boolean {
