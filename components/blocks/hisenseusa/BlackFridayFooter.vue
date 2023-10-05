e6cec134fe280ef46115b6fc7f223252799c9e3a
alexisponcedev <poncealexis640@gmail.com>
2023-10-05 11:03:16 +0300

    fix update tmeplate bug

--- components/blocks/hisenseusa/BlackFridayFooter.vue
+++ components/blocks/hisenseusa/BlackFridayFooter.vue
@@ -5,10 +5,10 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, VModel, Watch } from "vue-property-decorator";
-import { StructureType } from "~/models/StructureType";
-import blockAddItem from "~/utils/blockAddItem";
-import blockRemoveItem from "~/utils/blockRemoveItem";
+import { Vue, Component, Prop, VModel } from "vue-property-decorator";
+import { StructureType } from "@/models/StructureType";
+import blockRemoveItem from "@/utils/blockRemoveItem";
+import blockAddItem from "@/utils/blockAddItem";
 
 @Component
 export default class BlackFridayFooter extends Vue {
@@ -42,12 +42,14 @@ export default class BlackFridayFooter extends Vue {
                 },
             ],
         });
+
         blockAddItem(this.model, "rightText", {
             id: 2,
             type: StructureType.String,
             title: "Title",
             value: "",
         });
+
         if (this.model.template.value === "v2") {
             blockAddItem(this.model, "text_color", {
                 id: 3,
@@ -55,12 +57,14 @@ export default class BlackFridayFooter extends Vue {
                 title: "Text Color",
                 value: "#999999",
             });
+
             blockAddItem(this.model, "background_color", {
                 id: 4,
                 type: StructureType.Color,
                 title: "Background Color",
                 value: "#1a2a2d",
             });
+
             blockAddItem(this.model, "hover", {
                 id: 5,
                 type: StructureType.Select,
@@ -77,35 +81,24 @@ export default class BlackFridayFooter extends Vue {
                 "background_color",
                 "hover",
             ]);
+
+        this.model = { ...this.model };
     }
 
     mounted() {
         blockAddItem(this.model, "template", {
             id: 0,
-            type: StructureType.Select,
             title: "Template",
+            onChanged: this.prepare,
+            type: StructureType.Select,
             value: "v1",
             items: [
                 { title: "Version 1", value: "v1" },
                 { title: "Version 2", value: "v2" },
             ],
         });
+
         this.prepare();
-    }
-
-    get template() {
-        return this.model && this.model.template
-            ? this.model.template.value
-            : "";
-    }
-
-    @Watch("template")
-    onTemplateChanged() {
-        this.prepare();
-    }
-
-    get isEmpty(): Boolean {
-        return this.model && Object.keys(this.model).length === 0;
     }
 }
 </script>
