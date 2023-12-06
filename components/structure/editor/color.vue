61c4b604d70964c578b3f946867b7aa21208be4f
alexisponcedev <poncealexis640@gmail.com>
2023-12-06 12:35:14 +0330

    fix color picker bug

--- components/structure/editor/color.vue
+++ components/structure/editor/color.vue
@@ -4,20 +4,22 @@
             {{ model.title }}
         </label>
 
-        <div @click.prevent="show = true"
-             class="tw-text-center tw-w-full tw-mt-1 tw-flex tw-items-center tw-justify-between tw-px-2.5 tw-py-2 tw-bg-white tw-rounded">
+        <div
+            @click.prevent="show = true"
+            class="tw-text-center tw-w-full tw-mt-1 tw-flex tw-items-center tw-justify-between tw-px-2.5 tw-py-2 tw-bg-white tw-rounded"
+        >
             <div class="tw-whitespace-nowrap">Color Picker</div>
-            <div :style="{'backgroundColor' : model.value}"
-                 class="tw-flex tw-items-center tw-justify-center tw-py-2 tw-px-2 tw-min-w-40 tw-rounded-lg tw-shadow tw-text-center tw-uppercase">
-                {{ model.value }}
+            <div
+                :style="{ backgroundColor: color }"
+                class="tw-flex tw-items-center tw-justify-center tw-py-2 tw-px-2 tw-min-w-40 tw-rounded-lg tw-shadow tw-text-center tw-uppercase"
+            >
+                {{ color }}
             </div>
-
         </div>
 
         <v-dialog v-model="show" max-width="350">
-
             <v-card>
-                <v-card-title>Color Picker</v-card-title>
+                <v-card-title> Color Picker </v-card-title>
                 <v-card-text>
                     <v-color-picker
                         v-model="color"
@@ -30,28 +32,25 @@
 
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn @click.prevent="select" text color="green">Select</v-btn>
-                    <v-btn @click.prevent="close" text> Close</v-btn>
+                    <v-btn @click.prevent="select" text color="green">
+                        Select
+                    </v-btn>
+                    <v-btn @click.prevent="close" text> Close </v-btn>
                 </v-card-actions>
-
             </v-card>
-
-
         </v-dialog>
-
-
     </div>
 </template>
 
 <script lang="ts">
-import {Vue, Component, VModel, Watch} from "vue-property-decorator";
-import {StructureField} from "~/interfaces/StructureField";
+import { Vue, Component, VModel } from "vue-property-decorator";
+import { StructureField } from "@/interfaces/StructureField";
 
 @Component
 export default class StructureColorEditor extends Vue {
-    @VModel({type: StructureField}) model!: StructureField | any
+    @VModel({ type: StructureField }) model!: StructureField | any;
 
-    color: string = '#fff'
+    color: string = "#fff";
 
     show: Boolean = false;
 
@@ -64,14 +63,11 @@ export default class StructureColorEditor extends Vue {
         this.show = false;
     }
 
-    mounted(){
-        this.color = this.model.value;
+    mounted() {
+        this.color =
+            (typeof this.model.value == "object" && this.model.value.hexa) ||
+            this.model.value ||
+            "#fff";
     }
-
-    // @Watch('value')
-    // onValueChanged(color: string) {
-    //     this.model.value = color;
-    // }
-
 }
 </script>
