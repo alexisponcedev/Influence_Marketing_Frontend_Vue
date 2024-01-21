0b36af773068274c21bf42d308b6f9a918a4561a
alexisponcedev <poncealexis640@gmail.com>
2024-01-21 14:46:35 +0330

    add texts to footer menu

--- components/blocks/hisensecanada/Footer.vue
+++ components/blocks/hisensecanada/Footer.vue
@@ -6,22 +6,9 @@
 
 <script lang="ts">
 import { Vue, Component, VModel } from "vue-property-decorator";
-import { StructureType } from "@/models/StructureType";
-import blockAddItem from "@/utils/blockAddItem";
 
 @Component
 export default class Footer extends Vue {
     @VModel({ type: Object }) model!: any;
-
-    mounted() {
-        blockAddItem(this.model, "social-title", {
-            id: 1,
-            type: StructureType.String,
-            title: "Social Media Title",
-            value: "Follow Us",
-        });
-
-        this.model = { ...this.model };
-    }
 }
 </script>
