9e45dd2f4495ec49282f45b3be6aa4669ab983ea
alexisponcedev <poncealexis640@gmail.com>
2024-01-24 12:41:20 +0330

    updated

--- components/blocks/hisenseusa/BlogDescriptionBox.vue
+++ components/blocks/hisenseusa/BlogDescriptionBox.vue
@@ -1,57 +1,50 @@
 <template>
     <div>
-        <img src="/blocks/BlogDescriptionBox.png" alt=""/>
+        <img src="/blocks/BlogDescriptionBox.png" alt="" />
     </div>
 </template>
 
 <script lang="ts">
-import {Vue, Component, Prop, VModel, Watch} from "vue-property-decorator";
-import {StructureType} from "~/models/StructureType";
+import { Vue, Component, VModel } from "vue-property-decorator";
+import { StructureType } from "~/models/StructureType";
+import blockAddItem from "~/utils/blockAddItem";
 
 @Component
 export default class BlogDescriptionBox extends Vue {
-    @Prop(Number) readonly id: number | undefined
-    @Prop({default: true}) readonly editable: Boolean | undefined
-    @VModel({type: Object}) model!: any
-
-    reset(oldValue: any = {}) {
-
-        if (oldValue && Object.keys(oldValue).length > 0) {
-            this.model = {
-                ...oldValue, ...{
-                    backgroundColor: {id: 7, type: StructureType.Color, title: 'Background color', value: '#fff'}
-                }
-            }
-        } else
-            this.model = {
-                title: {id: 0, type: StructureType.String, title: 'Title', value: ''},
-                text: {id: 1, type: StructureType.Text, title: 'Text', value: ''},
-            }
-    }
+    @VModel({ type: Object }) model!: any
 
     mounted() {
-        if (this.isEmpty) this.reset();
+
+        blockAddItem(this.model, "template", {
+            id: 0,
+            type: StructureType.Select,
+            title: "Template",
+            itemText: "title",
+            itemValue: "value",
+            value: "",
+            items: [
+                { title: "Template 1", value: "template_1" },
+                { title: "Template 2", value: "template_2" },
+            ],
+        });
+
+        blockAddItem(this.model, "title", {
+            id: 1,
+            type: StructureType.String,
+            title: "Title",
+            value: "",
+        });
+
+        blockAddItem(this.model, "text", {
+            id: 2,
+            type: StructureType.Text,
+            title: "Text",
+            value: "",
+        });
+
+        this.model = { ...this.model };
     }
 
-    get isEmpty(): Boolean {
-        return this.model && Object.keys(this.model).length === 0;
-    }
 
-    addItem(name: string, item: any) {
-        if (!this.model.hasOwnProperty(name)) this.model[name] = item;
-        this.model[name].id = item.id;
-
-        if (this.model[name].type !== item.type) this.model[name].type = item.type;
-        if (item.type === StructureType.Image) {
-            this.model[name].src = '';
-            this.model[name].alt = 'Image Alt';
-        }
-        if (item.type === StructureType.List) {
-            this.model[name].newItem = item.newItem;
-        }
-        if (item.type === StructureType.Select) {
-            this.model[name].items = item.items;
-        }
-    }
 }
 </script>
